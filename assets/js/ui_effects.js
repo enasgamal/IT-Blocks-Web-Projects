8f75aa0d3c6d105c8499b7b457ea65f04e5ba462
enasgamal <enasgamalali98@gmail.com>
2021-01-17 00:57:15 +0200

    First commit

---
+++ assets/js/ui_effects.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    // Include Header Html
+    $("#header").loadHTML('../pages/header.html');
+    // Include Header2 Html
+    $("#header2").loadHTML('../pages/header2.html');
+    // Include Footer Html
+    $('#footer').loadHTML('../pages/footer.html');
+    // Include Home Html
+    $('#home').loadHTML('../pages/home.html');
+    // Include About Html
+    $('#about').loadHTML('../pages/about.html');
+});
+
+
+    
